f72a38262ae247a98597042d3129fcd1306dbc65
Blin Kukaj <kblin164@gmail.com>
2022-05-04 15:29:08 +0200

    --Putting controllers on actios and simplifying routeer

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,3 +1,39 @@
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    name: "Prishtina",
+    description: "The capital city of Kosovo, largest city in Kosovo.",
+    location: {
+      lat: 42.66066397045114,
+      lang: 21.175643842862385,
+    },
+    address: "Rruga Robert Doll, NR.158, 10000",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    name: "Gjakova",
+    description: "The oldest city and with a really rich culture on itself.",
+    location: {
+      lat: 42.38024874905271,
+      lang: 20.42714469838844,
+    },
+    address: "Xhamia e hadumit, 11000",
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    name: "Drenas",
+    description: "A city placed in Drenica's region.",
+    location: {
+      lat: 42.625027622866114,
+      lang: 20.895505802405385,
+    },
+    address: "Rruga e Jasharajve, NR.123, 13000",
+    creator: "u3",
+  },
+];
+
 const getPlaceById = (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => p.id === req.params.pid);
   if (!place) {
@@ -15,6 +51,20 @@ const getPlaceByUserId = (req, res, next) => {
     return next(new HttpError("Couldnt find a place by the provided user ID."));
   }
   res.json(place);
+};
+
+const createPlace = (req, res, next) => {
+  const place = {
+    id: `p`,
+    name: "Prishtina",
+    description: "The capital city of Kosovo, largest city in Kosovo.",
+    location: {
+      lat: 42.66066397045114,
+      lang: 21.175643842862385,
+    },
+    address: "Rruga Robert Doll, NR.158, 10000",
+    creator: "u1",
+  };
 };
 
 module.exports = { getPlaceById, getPlaceByUserId };
